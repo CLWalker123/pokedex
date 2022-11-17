f4049adc573ca0a1d13619f8bd1142a7b10e0cee
vytautasvargonas <vytautas.vargonas@gmail.com>
2022-11-17 10:32:38 +0200

    initial commit

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,18 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
+
+const myPokemonParty = ["Charmander", "Bulbasaur"];
 
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      {myPokemonParty.length &&
+        myPokemonParty.map((pokemon) => (
+          <View style={styles.pokemonContainer}>
+            <Text style={styles.pokemonTitle}>{pokemon}</Text>
+          </View>
+        ))}
     </View>
   );
 }
@@ -20,16 +20,35 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 24,
+    backgroundColor: "#f5ede6",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+
+  pokemonTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#1E252B",
+  },
+  pokemonContainer: {
+    height: 56,
+    width: "100%",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#1E252B",
+    backgroundColor: "#F8EADC",
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    marginBottom: 8,
   },
 });
---
+++ screens/PokemonListScreen.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Pressable, StyleSheet } from "react-native";
+
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+
+export default function PokemonListScreen({
+  navigation,
+}: RootTabScreenProps<"PokemonListScreen">) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.partyBtnWrapper}>
+        <Pressable
+          onPress={() => navigation.navigate("Modal")}
+          style={({ pressed }) => ({
+            opacity: pressed ? 0.5 : 1,
+          })}
+        >
+          <View style={styles.partyBtn}>
+            <Text style={{ fontSize: 10 }}>My Party</Text>
+          </View>
+        </Pressable>
+      </View>
+      <Pressable
+        onPress={() => navigation.navigate("TabTwo")}
+        style={({ pressed }) => ({
+          opacity: pressed ? 0.5 : 1,
+        })}
+      >
+        <View style={styles.pokemonContainer}>
+          <Text style={styles.pokemonTitle}>Pikachu</Text>
+        </View>
+      </Pressable>
+      <Pressable
+        onPress={() => navigation.navigate("TabTwo")}
+        style={({ pressed }) => ({
+          opacity: pressed ? 0.5 : 1,
+        })}
+      >
+        <View style={styles.pokemonContainer}>
+          <Text style={styles.pokemonTitle}>Charmander</Text>
+        </View>
+      </Pressable>
+      <Pressable
+        onPress={() => navigation.navigate("TabTwo")}
+        style={({ pressed }) => ({
+          opacity: pressed ? 0.5 : 1,
+        })}
+      >
+        <View style={styles.pokemonContainer}>
+          <Text style={styles.pokemonTitle}>Bulbasaur</Text>
+        </View>
+      </Pressable>
+      <Pressable
+        onPress={() => navigation.navigate("TabTwo")}
+        style={({ pressed }) => ({
+          opacity: pressed ? 0.5 : 1,
+        })}
+      >
+        <View style={styles.pokemonContainer}>
+          <Text style={styles.pokemonTitle}>Squirtle</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#f5ede6",
+  },
+  partyBtn: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: "#F8EADC",
+    marginRight: 16,
+    justifyContent: "center",
+    padding: 16,
+  },
+  partyBtnWrapper: {
+    paddingBottom: 24,
+    paddingTop: 8,
+    backgroundColor: "transparent",
+  },
+
+  pokemonTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#1E252B",
+  },
+  pokemonContainer: {
+    height: 56,
+    width: "100%",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#1E252B",
+    backgroundColor: "#F8EADC",
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    marginBottom: 8,
+  },
+});
